ba8aafae91a6e5ea7ad49d0a5c871962d48214fa
Mark Longchamps <mark_longchamps@outlook.com>
2021-01-12 17:14:30 -0500

    Show page is working

--- app/javascript/react/components/TaskShow.js
+++ app/javascript/react/components/TaskShow.js
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from "react"
 
 const TaskShow = (props) => {
-  debugger
-    const [task, setTask] = useState([])
+  
+    const [task, setTask] = useState({
+      body:"",
+      status:"",
+      task_starts_at:"",
+      time_worked:"",
+      timer_starts_at:"",
+      title:""
+
+    })
 
     const id = props.match.params.id 
 
     useEffect(() => {
-        fetch(`api/1/scripts/${id}`)
+      fetch(`/api/v1/tasks/${id}`)
         .then (response => {
-            if (response.ok) {
-              return response
+          if (response.ok) {
+            return response
             } else {
               let errorMessage = `${response.status} (${response.statusText})`,
                 error = new Error(errorMessage)
@@ -18,18 +26,89 @@ const TaskShow = (props) => {
             }
           })
           .then(response => response.json())
-          .then(body => {
+          .then(body => {debugger
             setTask(body)
             
           })
           .catch(error => console.error(`Error in fetch: ${error.message}`))
-          
+          debugger
         }, [])
+        
+      
+
+
   return(
-    <div>
-      This is the task show page
+    <>
+    <div className="grid-x grid-margin-x">
+      <div className = "medium-offset-1">
+        <div className="medium-3 callout">
+          
+        </div>
+      </div>
     </div>
+
+    <div className="grid-x grid-margin-x">
+      <div className="medium-offset-1 medium-6">
+        <table>
+          <thead>
+            <tr>
+              <th>TASK</th> 
+             </tr>
+            </thead>
+          <tbody>
+            {task.title}
+          </tbody>
+        </table>
+        <table>
+          <thead>
+            <tr>
+              <th>BODY</th>
+            </tr>
+          </thead>
+          <tbody>
+            {task.body}
+          </tbody>
+        </table>
+        <table>
+          <thead>
+            <tr>
+              <th>STATUS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {task.status}
+          </tbody>
+          <thead>
+            <tr>
+              <th>TASK STARTED AT</th>
+            </tr>
+          </thead>
+          <tbody>
+            {task.task_starts_at}
+          </tbody>
+          <thead>
+            <tr>
+              <th>TIME WORKED</th>
+            </tr>
+          </thead>
+          <tbody>
+            {task.time_worked}
+          </tbody>
+          <thead>
+            <tr>
+              <th>TIMER STARTS AT</th>
+            </tr>
+          </thead>
+          <tbody>
+            {task.timer_starts_at}
+          </tbody>
+          
+        </table>
+      </div>
+    </div>
+  </>
   )
 }
 
 export default TaskShow
+
