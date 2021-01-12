51a94bc1f8407f73b3b2e59f66ab14d11ff93460
jkoren <j.korenstein@gmail.com>
2021-01-12 16:47:45 -0500

    added table for hashtags

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  has_many :hashtags
 
   validates :title, presence: true
   validates :task_starts_at, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_193632) do
+ActiveRecord::Schema.define(version: 2021_01_12_204420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "hashtags", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "color", default: 0
+    t.bigint "task_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_hashtags_on_task_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
