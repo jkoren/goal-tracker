6e9cca132edcfe99128ce93c84c626834a054ce6
Ted Francis <tedward369@hotmail.com>
2021-01-13 17:35:18 -0500

    Search Feature Attempts

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,17 +8,4 @@ class Task < ApplicationRecord
   enum status: { "To Do": 1, Doing: 2, Done: 3}
   # see https://naturaily.com/blog/ruby-on-rails-enum
 
-  def self.search(search)
-
-    if search
-      task = Task.find_by(name: search)
-      if task
-        where(task_id: search)
-      else
-        all
-      end
-    else
-      all
-    end
-  end
 end
