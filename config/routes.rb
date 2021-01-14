7087bf061c057bb15259677b50fbbf199914a1c6
jkoren <j.korenstein@gmail.com>
2021-01-13 20:53:06 -0500

    set up endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
-  # get 'task/create'
-  # get 'task/show'
-  # get 'task/destroy'
-  # get 'task/update'
   root 'homes#index'
   devise_for :users
 
   get "/tasks/:id", to: "homes#index"
   get "/tasks", to: "homes#index"
-
+  
   namespace :api do
     namespace :v1 do
+      get "/search/:term", to: "tasks#search"
       resources :tasks, only: [:index, :show, :create, :edit, :destroy, :update]
     end
   end
