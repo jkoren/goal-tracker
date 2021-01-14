a5cc819d49b0dc6549068d1c679ae1216db0ed31
jkoren <j.korenstein@gmail.com>
2021-01-14 09:14:06 -0500

    stopwatch started, added styling, logo, more seed data

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -2,6 +2,6 @@ class Hashtag < ApplicationRecord
   belongs_to :task
 
   validates :title, presence: true
-  enum color: { yellow: 0, blue: 1, green: 2, red: 3}
+  enum color: { yellow: 0, blue: 1, green: 2, red: 3, purple: 4}
   # see https://naturaily.com/blog/ruby-on-rails-enum
 end
