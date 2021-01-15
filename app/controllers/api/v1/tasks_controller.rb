755ac9b015ff0a3b72505f2c267e5500e5a673d3
jkoren <j.korenstein@gmail.com>
2021-01-15 00:28:03 -0500

    fixed edit function

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -2,32 +2,30 @@ class Api::V1::TasksController < ApplicationController
   # before_action :authenticate_user!
   protect_from_forgery unless: -> { request.format.json? }
   skip_before_action :verify_authenticity_token, only: [:create, :update]
- 
-  # before_action :authorize_user, except: [:index, :show]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-
+  
+  # before_action :authorize_user, except: [:index, :show]
   # before_action :authenticate_user, except: [:index, :show]
   # before_action :authorize_user, except: [:index, :show, :create]
 
   # GET /tasks
   def index
-    @tasks = current_user.tasks
-    @tasks = @tasks.sort_by{ |a| a[:created_at] }.reverse
-    render json: @tasks, each_serializer: TaskSerializer
+    tasks = current_user.tasks
+    tasks = tasks.sort_by{ |a| a[:created_at] }.reverse
+    render json: tasks, each_serializer: TaskSerializer
   end
 
   # GET /tasks/1
   def show
-    
-    @tasks = Task.find(params[:id])
-    render json: @tasks, serializer: TaskSerializer
+    tasks = Task.find(params[:id])
+    render json: tasks, serializer: TaskSerializer
   end
 
   def search
     @term = params[:term].downcase
-    @tasks = Task.all
+    tasks = Task.all
     selectedTasks = []
-    @tasks.each do | task |
+    tasks.each do | task |
       found = false
       found = task.title.downcase.include?(@term)
       if !found
@@ -51,15 +49,15 @@ class Api::V1::TasksController < ApplicationController
 
   # POST /tasks
   def create
-    @task = Task.new(task_params)
-    @task.user = current_user
+    task = Task.new(task_params)
+    task.user = current_user
     @time_int = task_params["task_starts_at"]
-    @task.task_starts_at = @time_int.to_datetime
-    if @task.save
+    task.task_starts_at = @time_int.to_datetime
+    if task.save
       flash[:notice] = 'Task was successfully created.'
-      render json: @task
+      render json: task
     else 
-      render json: @task, status: :error
+      render json: task, status: :error
       # https://medium.com/rails-ember-beyond/error-handling-in-rails-the-modular-way-9afcddd2fe1b
       
       # render json: {status: "error", code: 3000, message: "User is not logged in"}
@@ -69,10 +67,20 @@ class Api::V1::TasksController < ApplicationController
 
   # PATCH/PUT /tasks/1
   def update
-    @task = Task.find_by(id: params[:id])
-
-    if @task.update(task_params)
-      redirect_to @tasks, notice: 'Task was successfully updated.'
+    task = Task.find_by(id: params[:id])
+    # rails update needs to have process as value, not text
+    if task_params["status"]  == "To Do"
+      task_num = 1
+    elsif task_params["status"]  == "In Progress"
+      task_num = 2
+    else
+      task_num = 3
+    end
+    revised_params = task_params
+    revised_params["status"] = task_num
+ 
+    if task.update(revised_params)
+      redirect_to @root
     else
       render :edit
     end
@@ -80,8 +88,8 @@ class Api::V1::TasksController < ApplicationController
 
   # DELETE /tasks/1
   def destroy
-    @task = Task.find(params[:id])
-    if @task.destroy
+    task = Task.find(params[:id])
+    if task.destroy
       render json: {destroyed: true}
     end   
   end
@@ -97,7 +105,7 @@ end
 private
   # Use callbacks to share common setup or constraints between actions.
   def set_task
-    @task = Task.find(params[:id])
+    task = Task.find(params[:id])
   end
 
   def authenticate_user
@@ -108,5 +116,5 @@ private
 
     # Only allow a trusted parameter "white list" through.
     def task_params
-      params.require(:task).permit(:title, :body, :task_starts_at, :timer_starts_at, :time_worked, :status, :search)
+      params.require(:task).permit(:id, :title, :body, :task_starts_at, :timer_starts_at, :time_worked, :status, :search)
     end
